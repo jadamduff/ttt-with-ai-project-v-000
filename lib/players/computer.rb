6587e0b67f7bbfaa2a969ead82e3730efac6b73b
Adam Duff <j.adam.duff@gmail.com>
2018-08-20 07:12:19 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,12 +6,12 @@ module Players
   class Computer < Player
 
     def test_opp_gap(board)
-      test = WIN_COMBINATIONS.find {|combo| !board.cells[combo[0]] == self.token && !board.cells[combo[2]] == self.token && board.taken?([combo[0]]) && board.taken?([combo[2]]) && !board.taken?([combo[1]])}
+      test = WIN_COMBINATIONS.find {|combo| !board.cells[combo[0]] == self.token && !board.cells[combo[2]] == self.token && board.taken?(combo[0]) && board.taken?(combo[2]) && !board.taken?(combo[1])}
       test ? test.first[2].to_s : false
     end
 
     def test_own_gap(board)
-      test = WIN_COMBINATIONS.find {|combo| board.cells[combo[0]] == self.token && board.cells[combo[2]] == self.token && !board.taken?([combo[1]])}
+      test = WIN_COMBINATIONS.find {|combo| board.cells[combo[0]] == self.token && board.cells[combo[2]] == self.token && !board.taken?(combo[1])}
       test ? test.first[2].to_s : false
     end
 
