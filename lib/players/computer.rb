ecbf7f47c9b1e2e917b56f64fdeb2da88516d61c
Adam Duff <j.adam.duff@gmail.com>
2018-08-20 06:19:29 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,7 +8,7 @@ module Players
     def move(board)
       WIN_COMBINATIONS.each do |combo|
         if board.cells[combo[0]] == self.token && board.cells[combo[2]] == self.token
-          return (combo[1] + 1)
+          return combo[1] + 2
         end
       end
       ORDER.each do |cell|
